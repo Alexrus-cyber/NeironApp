4f0cf1979a87b9cd1688fbfccdea0e93fa2ae036
Alexrus-cyber <85308640+Alexrus-cyber@users.noreply.github.com>
2022-03-23 11:09:19 +0300

    New Version, no bag

---
+++ ConsoleAp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
